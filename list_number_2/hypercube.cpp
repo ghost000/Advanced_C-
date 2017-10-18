4e2f8772ae6125d087b2426aca2932fdc20e3973
ghost000 <radixblade100@gmail.com>
2017-10-18 09:10:07 +0200

    I added hypercube.cpp file with solutions of task 3 from list number 2

---
+++ list_number_2/hypercube.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+//template<int Length, int Dimension>
+//class HyperCube;
+
+template<int Length, int Dimension>
+class HyperCube{
+	public:
+		enum{ VALUE = Length * HyperCube< Length, Dimension - 1 >::VALUE };
+};
+
+template<int Length>
+class HyperCube< Length, 1 >{
+	public: 
+		enum{ VALUE = Length };
+};
+
+
+int main(int argv, char * argc[]){
+
+	std::cout << HyperCube<10,5>::VALUE << std::endl;
+	return 0;
+}
